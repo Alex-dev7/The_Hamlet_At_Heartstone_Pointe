1f3943c4a88f38e1f7a3cb26d4514151f3df100e
Alexei Rusu <rusualexei1995@gmail.com>
2023-07-15 12:05:50 -0400

    initial commit

---
+++ app/(site)/components/Nav.tsx
@@ -0,0 +1,12 @@
+export default function Nav(){
+    return (
+        <nav>
+            <a href="#">
+                About
+            </a>
+            <a href="#">
+                Contact
+            </a>
+        </nav>
+    )
+}
