9e15e18a3d699eb36a8c4993d9ce6706cb327205
Alexei Rusu <rusualexei1995@gmail.com>
2023-08-30 23:34:23 -0400

    form/logic

--- app/(site)/contact/page.tsx
+++ app/(site)/contact/page.tsx
@@ -12,7 +12,7 @@ const initState = {values: initValues}
 function Contact() {
 const [state, setState] = useState(initState)
  
-const {values} = state
+const {values, isLoading} = state
 
 const handleChange = ({target}) => setState((prev) => ({
   ...prev,
@@ -22,37 +22,71 @@ const handleChange = ({target}) => setState((prev) => ({
   }
 }) ) 
 
+const onSubmit = async (e) => {
+  e.preventDefault()
+  console.log(e)
+  setState((prev) => ({
+    ...prev,
+    isLoading: true
+  }))
+}
+
   return (
     <div className="h-screen w-screen flex place-items-center justify-evenly bg-[#314F44] ">
-        <div className="w-[50%] h-[90%] outline ">
-            <p>
-                hi
-            </p>
-        </div>
-        <div className="w-[50%] h-[90%] outline " >
-          <form 
-          className="w-[600px] h-fit mx-auto flex flex-col gap-10 outline p-6 my-[50%]"> 
-            <fieldset className="flex flex-col gap-2 text-white font-thin">
-              {/* <label>Name</label> */}
-              <input type="text" name="name" placeholder="Name"  required value={values.name} onChange={handleChange}
-              className="w-full py-6 bg-transparent border-b-[1px] border-white focus:outline-none focus:border-cyan-300"/>
-            </fieldset>
-            <fieldset className="flex flex-col gap-2 text-white font-thin">
-              {/* <label>Email</label> */}
-              <input type="email" name="email" placeholder="Email" required value={values.email} onChange={handleChange}
-              className="w-full py-6 bg-transparent border-b-[1px] border-white focus:outline-none focus:border-cyan-300"/>
-            </fieldset>
-            <fieldset className="flex flex-col gap-2 text-white font-thin">
-              {/* <label>Email</label> */}
-              <textarea  name="message" rows={4} placeholder="Type your message here..." required value={values.message} onChange={handleChange}
-              className="w-full py-6 bg-transparent border-b-[1px] border-white focus:outline-none focus:border-cyan-300"/>
-            </fieldset>
+      <div className="w-[50%] h-[90%] outline ">
+        <p>hi</p>
+      </div>
+      <div className="w-[50%] h-[90%] outline ">
+        <form className="w-[600px] h-fit mx-auto flex flex-col gap-10 outline p-6 my-[50%]">
+          <fieldset className="flex flex-col gap-2 text-white font-thin text-sm">
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              required
+              value={values.name}
+              onChange={handleChange}
+              className="w-full py-6 bg-transparent border-b-[1px]   placeholder-white border-white focus:outline-none focus:border-cyan-300 "
+            />
+          </fieldset>
 
-          </form>
-        </div>
-     py-6
+          <fieldset className="flex flex-col gap-2 text-white font-thin">
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              required
+              value={values.email}
+              onChange={handleChange}
+              className="w-full py-6 bg-transparent border-b-[1px]  placeholder-white border-white focus:outline-none focus:border-cyan-300"
+            />
+          </fieldset>
+
+          <fieldset className="flex flex-col gap-2 text-white font-thin">
+            <textarea
+              name="message"
+              rows={4}
+              placeholder="Type your message here..."
+              required
+              value={values.message}
+              onChange={handleChange}
+              className="w-full py-6 bg-transparent border-b-[1px]  placeholder-white border-white focus:outline-none focus:border-cyan-300"
+            />
+          </fieldset>
+          <input
+            type="submit"
+            disabled={!values.name || !values.email || !values.message}
+            // isLoading={isLoading}
+            onClick={onSubmit}
+            className="bg-neutral-200 w-fit px-12 py-3 font-extralight text-sm place-self-end hover:bg-white"
+          />
+            {/* SUBMIT
+          </input> */}
+        </form>
+      </div>
+      py-6
     </div>
-  )
+  );
 }
 
 export default Contact
