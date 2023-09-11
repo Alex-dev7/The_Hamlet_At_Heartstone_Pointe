3e0fdb33945ee9725b9839a35e596d46a6c5562e
Alexei Rusu <rusualexei1995@gmail.com>
2023-09-11 18:55:17 -0400

    fetching data for the exterior page

--- app/(site)/exterior/page.tsx
+++ app/(site)/exterior/page.tsx
@@ -4,6 +4,7 @@ import { motion } from "framer-motion"
 import { useEffect, useState } from "react"
 import { getInteriorInfo } from "@/sanity/sanity-utils"
 import { Interior } from "@/types/Interior"
+import { PortableText } from "@portabletext/react"
 
 function Exterior() {
   const [data, setData] = useState<Interior[] | {}>()
@@ -18,16 +19,17 @@ function Exterior() {
        })
   }, [])
 
-    function handleClick(e: any) {
-      // console.log(e.target.src)
-      setOpenModal(!openModal)
-      setImg(e.target.src)
-      return 0
-    }
-    function closeModal(){
-      setOpenModal(false)
-      return 0
-    }
+  function handleClick(e: any, index: number){
+    console.log(e.target)
+    setOpenModal(!openModal)
+    setImg(index)
+    return 0
+
+  }
+  function closeModal(){
+    setOpenModal(false)
+    return 0
+  }
 
   return (
     <motion.section className="min-h-screen py-[10%] px-[2%] grid grid-cols-2 "
@@ -38,17 +40,19 @@ function Exterior() {
     > 
     { openModal &&
       <div onClick={closeModal} className="fixed flex justify-center place-items-center z-50 bg-black bg-opacity-75 top-0 right-0 w-screen h-full">
-        { img && <Image src={`https://picsum.photos/800/400`} alt="" width={800} height={400} className=""/>}
+        <span className="font-normal absolute right-10 top-10 text-[2rem] hover:scale-125 transition-all cursor-pointer">X</span>
+        {/* @ts-ignore: Unreachable code error */}
+        { data && <Image src={data.images[img]} alt="" width={500} height={400} className="h-auto"/>}
 
       </div>
     }
 
         <div className="grid grid-cols-2 gap-5 grid-flow-row ">
-           
-        {images.map((i) => i % 3 === 0 ?  
-         <Image key={i} onClick={handleClick} className="col-span-2 w-full  object-cover outline  border-4 border-white hover:scale-[102%] transition-all brightness-75 hover:brightness-100"  src="https://picsum.photos/800/400" width={800} height={400} alt="#" /> 
+          {/* @ts-ignore: Unreachable code error */}  
+          {data && data?.images.map((image: string, index: number) => index  % 3 === 0 ?  
+         <Image key={index}  onClick={(e) => handleClick(e, index)} className="col-span-2 w-full  object-cover outline  border-4 border-white hover:scale-[102%] transition-all brightness-75 hover:brightness-100 cursor-pointer"  src={image} width={800} height={400} alt="#" /> 
          : 
-         <Image key={i} onClick={handleClick} src="https://picsum.photos/600/800" alt="#" width={600} height={800} className="border-4 border-white hover:scale-[102%] transition-all brightness-75 hover:brightness-100"/>
+         <Image key={index}  onClick={(e) => handleClick(e, index)} src={image} alt="#" width={600} height={800} className="border-4 h-full object-cover border-white hover:scale-[102%] transition-all brightness-75 hover:brightness-100 cursor-pointer"/>
                )}
              
        </div>
@@ -56,9 +60,8 @@ function Exterior() {
             <div className="fixed w-[45%] top-[20%] right-5 flex flex-col gap-10 ">
                 <h3 className="text-end mr-10 text-[2.8rem]">exterior.</h3>
                 <hr/>
-                <p className="text-[1.1rem]">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p> 
+                {/* @ts-ignore: Unreachable code error */}  
+                <PortableText value={data && data?.content} /> 
                 <Image src={"https://i.imgur.com/RQgw2hA.jpeg"} alt=""  width={600} height={800} className="w-[900px] "/>                 
             </div>
         </div>
--- app/(site)/interior/page.tsx
+++ app/(site)/interior/page.tsx
@@ -2,7 +2,6 @@
 import Image from "next/image"
 import { motion } from "framer-motion"
 import { useEffect, useState } from "react"
-
 import { getInteriorInfo } from "@/sanity/sanity-utils"
 import { PortableText } from "@portabletext/react"
 import { Interior } from "@/types/Interior"
@@ -44,6 +43,7 @@ function Interior() {
     >
           { openModal &&
       <div onClick={closeModal} className="fixed flex justify-center place-items-center z-50 bg-black bg-opacity-75 top-0 right-0 w-screen h-full">
+        <span className="font-normal absolute right-10 top-10 text-[2rem] hover:scale-125 transition-all cursor-pointer">X</span>
       {/* @ts-ignore: Unreachable code error */}
         { data && <Image src={data.images[img]} alt="" width={500} height={400} className="h-auto"/>}
 
@@ -61,9 +61,9 @@ function Interior() {
         <div className="grid grid-cols-2 gap-2 grid-flow-row ">
           {/* @ts-ignore: Unreachable code error */}  
         {data && data?.images.map((image: string, index: number) => index  % 3 === 0 ?  
-         <Image key={index}  onClick={(e) => handleClick(e, index)} className="col-span-2 w-full  object-cover outline  border-4 border-white hover:scale-[102%] transition-all brightness-75 hover:brightness-100"  src={image} width={800} height={400} alt="#" /> 
+         <Image key={index}  onClick={(e) => handleClick(e, index)} className="col-span-2 w-full  object-cover outline  border-4 border-white hover:scale-[102%] transition-all brightness-75 hover:brightness-100 cursor-pointer"  src={image} width={800} height={400} alt="#" /> 
          : 
-         <Image key={index}  onClick={(e) => handleClick(e, index)} src={image} alt="#" width={600} height={800} className="border-4 h-auto border-white hover:scale-[102%] transition-all brightness-75 hover:brightness-100"/>
+         <Image key={index}  onClick={(e) => handleClick(e, index)} src={image} alt="#" width={600} height={800} className="border-4 h-full object-cover border-white hover:scale-[102%] transition-all brightness-75 hover:brightness-100 cursor-pointer"/>
                )}
         </div>
 
