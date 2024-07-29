7dd171bc22b2c47a59f62e0fc2b4bba7581f027e
Alexei Rusu <rusualexei1995@gmail.com>
2024-07-29 19:44:15 -0400

    updated package.json file

--- next.config.js
+++ next.config.js
@@ -1,7 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['picsum.photos', 'i.imgur.com', 'images.unsplash.com', "cdn.sanity.io"]
+        // remotePatterns: ['picsum.photos', 'i.imgur.com', 'images.unsplash.com', "cdn.sanity.io"]
+        remotePatterns: [
+                {
+                    protocol: 'https',
+                    hostname: 'picsum.photos',
+                    // port: '',
+                    // pathname: '/account123/**',
+                },
+                {
+                    protocol: 'https',
+                    hostname: 'i.imgur.com',
+                    // port: '',
+                    // pathname: '/account123/**',
+                },
+                {
+                    protocol: 'https',
+                    hostname: 'images.unsplash.com',
+                    // port: '',
+                    // pathname: '/account123/**',
+                },
+                {
+                    protocol: 'https',
+                    hostname: 'cdn.sanity.io',
+                    // port: '',
+                    // pathname: '/account123/**',
+                },
+        ]
     },
 }
 
